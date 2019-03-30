73108d9403854a7c18127e257e481979e709d29a
Sam <sam210723@hotmail.com>
2019-03-30 14:58:50 +1000

    Create lrit-demux.py

---
+++ demux/lrit-demux.py
@@ -0,0 +1,11 @@
+"""
+lrit-demux.py
+https://github.com/sam210723/COMS-1
+
+De-multiplexes LRIT downlink into LRIT files.
+"""
+
+import argparse
+
+argparser = argparse.ArgumentParser(description="De-multiplexes LRIT downlink into LRIT files.")
+args = argparser.parse_args()
