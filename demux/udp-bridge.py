34255d70bad87e7ec6e2149208c674cb1489677f
Sam <sam210723@hotmail.com>
2019-03-30 17:07:54 +1000

    UDP to TCP bridge
    Workaround for broken TCP Sinks in GNU Radio for Windows

---
+++ demux/udp-bridge.py
@@ -0,0 +1,78 @@
+"""
+udp-bridge.py
+https://github.com/sam210723/COMS-1
+
+Uni-directional UDP to TCP bridge.
+"""
+
+import argparse
+import socket
+
+argparser = argparse.ArgumentParser(description="Uni-directional UDP to TCP bridge.")
+argparser.add_argument("UDP", action="store", help="UDP port")
+argparser.add_argument("TCP", action="store", help="TCP port")
+args = argparser.parse_args()
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = int(args.UDP)
+TCP_IP = "127.0.0.1"
+TCP_PORT = int(args.TCP)
+
+udpSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def startUDP():
+    print("Starting UDP...")
+
+    try:
+        udpSocket.bind((UDP_IP, UDP_PORT))
+    except socket.error as e:
+        if e.errno == 10048:
+            print("PORT {} ALREADY IN USE".format(UDP_PORT))
+        else:
+            print(e)
+        
+        print("Exiting...\n")
+        exit()
+    
+    print("UDP OK\n")
+
+
+def startTCP():
+    print("Starting TCP...")
+
+    try:
+        tcpSocket.connect((TCP_IP, TCP_PORT))
+    except socket.error as e:
+        if e.errno == 10061:
+            print("TCP CONNECTION REFUSED".format())
+        else:
+            print(e)
+        
+        print("Exiting...\n")
+        exit()
+
+    print("TCP OK\n")
+
+
+def bridge():
+    print("Bridge connected")
+
+    while True:
+        data, addr = udpSocket.recvfrom(1024)
+        tcpSocket.send(data)
+        
+
+def init():
+    print()
+    print("UDP Port: {}".format(args.UDP))
+    print("TCP Port: {}".format(args.TCP))
+    print()
+
+    startUDP()
+    startTCP()
+
+    bridge()
+    
+
+init()
