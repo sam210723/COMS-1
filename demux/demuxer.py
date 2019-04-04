33d9dbb00341cb467a7d0096a79c0ed10b3a6b11
Sam <sam210723@hotmail.com>
2019-04-04 22:27:15 +1000

    New VCDU class

--- demux/demuxer.py
+++ demux/demuxer.py
@@ -1,16 +1,23 @@
-class Demuxer:
-    """
-    Coordinates CCSDS demuxing from VCDU layer to xRIT file layer.
-    """
+"""
+demuxer.py
+https://github.com/sam210723/COMS-1
 
+Coordinates CCSDS demuxing from VCDU layer to xRIT file layer.
+"""
+
+from VCDU import VCDU
+
+class Demuxer:
+    
     def __init__(self, mode):
         """
         Initialise Demuxer class
-        :param mode: Specify LRIT specify or HRIT mode
+        :param mode: Specify LRIT or HRIT mode
         """
 
         self.mode = mode
 
 
     def data_in(self, data):
-        
+        # Parse VCDU
+        currentVCDU = VCDU(data)
--- demux/tools.py
+++ demux/tools.py
@@ -19,6 +19,21 @@ def get_bits(data, start, length, count):
     return bits
 
 
+def get_bits_int(data, start, length, count):
+    """
+    Get bits from bytes as integer
+
+    :param data: Bytes to get bits from
+    :param start: Start offset in bits
+    :param length: Number of bits to get
+    :param count: Total number of bits in bytes (accounts for leading zeros)
+    """
+
+    bits = get_bits(data, start, length, count)
+
+    return int(bits, 2)
+
+
 def new_dir_exists(path):
     """
     Create new directory if it doesn't exist already
