980eba62b8f373b49f93b64ba79110caab2bd315
Sam <sam210723@hotmail.com>
2019-05-17 23:11:22 +1000

    Source image detection

--- overlay.py
+++ overlay.py
@@ -6,6 +6,7 @@ Adds overlays and text to COMS-1 Meteorological Imager images.
 """
 
 import argparse
+from PIL import Image, ImageDraw
 
 argparser = argparse.ArgumentParser(description="Adds overlays and text to COMS-1 Meteorological Imager images.")
 argparser.add_argument("INPUT", action="store", help="LRIT file to process")
@@ -16,8 +17,31 @@ argparser.add_argument("-tc", action="store", help="Text colour", default="white
 args = argparser.parse_args()
 
 def init():
-    return
+    # Print arguments
+    if args.tl != None:
+        print("LEFT TEXT:   \"{}\"".format(args.tl))
+    if args.tr != None:
+        print("RIGHT TEXT:  \"{}\"".format(args.tr))
 
+    # Load input image
+    sourceImage = Image.open(args.INPUT)
+    sourceWidth = sourceImage.width
+    sourceHeight = sourceImage.height
+    outputImage = Image.new("RGB", (sourceWidth, sourceHeight))
+    outputImage.paste(sourceImage, (0, 0))
+    draw = ImageDraw.Draw(outputImage)
+
+    if sourceHeight == 2200 and sourceWidth == 2200:
+        imageType = "Full Disk (FD)"
+    elif sourceWidth == 1547 and sourceHeight == 1234:
+        imageType = "Enhanced Northern Hemisphere (ENH)"
+    elif sourceWidth == 1547 and sourceHeight == 636:
+        imageType = "Limited Southern Hemisphere (LSH)"
+    elif sourceWidth == 810 and sourceHeight == 611:
+        imageType = "Asia-Pacific Northern Hemisphere (APNH)"
+    else:
+        imageType = "UNRECOGNISED"
+    print("IMAGE TYPE:  {}".format(imageType))
 
 try:
     init()
